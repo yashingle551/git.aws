de44e514fd141585236ebab5051ce645e3e04c2c
yashingle551 <yingle360@gmail.com>
2025-10-12 15:49:35 +0530

    Create cleanup.sh

---
+++ Scripts/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#set -e
+
+##TARGET_DIR="/home/ec2-user/my-python-app"
+
+echo "Cleaning old files"
+##rm -rf "$TARGET_DIR"/*
+rm -rf "/home/ec2-user/my-python-app"/*
+echo "Cleanup complete."
